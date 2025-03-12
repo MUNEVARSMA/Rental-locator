53e5e2f0eb6082bc3595c0576e2b921933fcd43b
MUNEVARSMA <alejandromunevar716@gmail.com>
2025-03-12 14:18:43 -0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,8 @@
+function responsiveMenu() {
+    const nav = document.getElementById('nav');
+    if (nav.style.display === 'block') {
+        nav.style.display = 'none';
+    } else {
+        nav.style.display = 'block';
+    }
+}
